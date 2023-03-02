1b5e38532273fce3fbe9a4204c49f19c12398810
alberthaciverdiyev1 <7cw5lnl@code.edu.az>
2023-03-02 19:34:39 +0400

    new changes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum1 = 10;
+            int sum2 = 20;
+            int sum=sum1+sum2;
+            bool dead=false;
+            float a = 2.11f;
+            string UserName = "Albert";
+            string Password = "88888888";
+            
+            double b = 2.88d;
+            double c = a + b;
+            Console.WriteLine(c);
+            Console.WriteLine(UserName +" -- "+ Password);
+            Console.WriteLine(sum);
+            Console.WriteLine(sum1!=sum2);
+            Console.WriteLine(sum2>sum1 && a>b);
+            Console.WriteLine(dead);
+           
+        }
+    }
+}
