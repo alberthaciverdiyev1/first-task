0c427beb57b33b964795e58071d9a8c50cb28993
alberthaciverdiyev1 <7cw5lnl@code.edu.az>
2023-03-02 13:51:23 +0400

    first

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace FirstTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=30;
+            int b=20;
+            string c = "salam necesen ?";
+            string p = "sagol yaxsiyam";
+         
+            char d = '%';
+            float y = 2.31f;
+            double k = 97.25d;
+            int e = a + b;
+            int r = a - b;
+            double t = k + y;
+            string o = "c" + "p";
+            
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);   
+            Console.WriteLine(d);
+            Console.WriteLine(y);
+            Console.WriteLine(k);
+            Console.WriteLine(e);
+            Console.WriteLine(r);
+            Console.WriteLine(t);
+
+
+
+          
+        }
+    }
+}
